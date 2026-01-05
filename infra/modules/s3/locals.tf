c32b8ef611070301023d6eed4c8db519685ffadd
artamim <artamim22@gmail.com>
2026-01-05 16:36:16 +0600

    Updated routes

--- infra/modules/s3/locals.tf
+++ infra/modules/s3/locals.tf
@@ -1,8 +1,6 @@
 locals {
   routing_rules = [
-    {
-      prefix           = "contact"
-      replace_with     = "contact.html"
-    },
+    { prefix = "contact",  replace_with = "contact-page.html" },
+    { prefix = "contact/", replace_with = "contact-page.html" },
   ]
 }
